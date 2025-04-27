17c1abf7c14b382ab5f4ec125998fcf6e0d3c2aa
AlexisMa6 <kay634511@gmail.com>
2025-04-27 23:00:54 +0200

    on est finito

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
 import random
+import sqlite3
 app = Flask(__name__)
 
 def get_chaussure_par_filtre(*filtre) :
     print(filtre)
     return [
         {
-            'nom': 'New Balance 574',
-            'prix': 90,
-            'couleur': 'Bleu',
-            'image': '/static/images/nb574.jpg',
-            'lien': 'https://www.newbalance.fr/nb574',
-            'taille': '41-44'
-        },
-        {
-            'nom': 'New Balance 997',
+            'nom': 'New Balance 9060 ',
             'prix': 130,
-            'couleur': 'Noir',
-            'image': '/static/images/nb997.jpg',
-            'lien': 'https://www.newbalance.fr/nb997',
+            'couleur': 'Marron',
+            'image': 'https://images.stockx.com/images/New-Balance-9060-Mushroom-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=57&dpr=2&trim=color&updated_at=1738193358',
+            'lien': 'https://limitedresell.com/4024-new-balance-9060-mushroom.html?variant=75017&epik=dj0yJnU9cEFNWHVEcDh3SzJKS1lqeDQzZFpTUi14WVhoNXVrZnUmcD0wJm49cEtOOEdsQkNvYmhLM0UwaXBGUmdZQSZ0PUFBQUFBR2dPR253',
             'taille': '42-45'
         },
         {
-            'nom': 'New Balance 990',
+            'nom': 'TN Tiempo ',
+            'prix': 130,
+            'couleur': 'rouge',
+            'image': 'https://images.stockx.com/images/Nike-Air-Max-Plus-Tiempo-Team-Red-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=57&dpr=2&trim=color&updated_at=1738193358',
+            'lien': 'https://stockx.com/fr-fr/nike-air-max-plus-tiempo-team-red?country=FR&currencyCode=EUR&size=14&utm_source=af&utm_medium=imp&utm_campaign=2213966&impactSiteId=Rq03XSUC-xyKWFvXtoQH2SVcUksRkYRREW4MWA0&clickid=Rq03XSUC-xyKWFvXtoQH2SVcUksRkYRREW4MWA0&utm_term=Rq03XSUC-xyKWFvXtoQH2SVcUksRkYRREW4MWA0&utm_content=_1023691&irgwc=1&irclickid=Rq03XSUC-xyKWFvXtoQH2SVcUksRkYRREW4MWA0&ir_campaignid=9060&ir_adid=1023691&ir_partnerid=2213966',
+            'taille': '42-45'
+        },
+        {   'nom': 'Shox R4 floral',
+            'prix': 90,
+            'couleur': 'Beige',
+            'image': 'https://images.stockx.com/images/Nike-Shox-R4-Flat-Pewter-Floral-Womens.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=57&dpr=2&trim=color&updated_at=1738266672',
+            'lien': 'https://www.nike.com/fr/t/chaussure-shox-r4-pour-wQWPrJH5/HV0934-001',
+            'taille': '41-44'
+        },
+        {
+            'nom': 'Nike Air Max A-COLD-WALL ',
+            'prix': 130,
+            'couleur': 'Bleu',
+            'image': 'https://images.stockx.com/images/Nike-Air-Max-Plus-A-COLD-WALL-Varsity-Royal-Product.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=57&dpr=2&trim=color&updated_at=1738193358',
+            'lien': 'https://stockx.com/fr-fr/nike-air-max-plus-a-cold-wall-varsity-royal?msockid=3232c88822cb6ec001a3dcdf23726f79',
+            'taille': '42-45'
+        },
+        { 
+            'nom': 'Nike Initiator',
             'prix': 180,
-            'couleur': 'Gris',
-            'image': '/static/images/nb990.jpg',
-            'lien': 'https://www.newbalance.fr/nb990',
+            'couleur': 'Blanc ',
+            'image': 'https://images.stockx.com/images/Nike-Initiator-White-Photon-Dust-Womens.jpg?fit=fill&bg=FFFFFF&w=140&h=75&q=57&dpr=2&trim=color&updated_at=1736779954',
+            'lien': 'https://www.nike.com/fr/t/chaussure-initiator-pour-pHUePZkd/FZ9020-100',
             'taille': '40-43'
         }
     ]
